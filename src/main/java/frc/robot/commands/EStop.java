a7283007c0459a091a4c758c335b89e270152001
IronGG <74273030+IronGGDev@users.noreply.github.com>
2021-11-21 20:29:44 -0500

    Update EStop

--- src/main/java/frc/robot/commands/EStop.java
+++ src/main/java/frc/robot/commands/EStop.java
@@ -17,9 +17,7 @@ public class EStop extends CommandBase {
      * Creates a new ExampleCommand.
      *
      */
-    public EStop() {
-
-    }
+    public EStop() {}
 
 
     // Called when the command is initially scheduled.
@@ -38,22 +36,17 @@ public class EStop extends CommandBase {
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {
-
-    }
+    public void execute() {}
 
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-
-    }
+    public void end(boolean interrupted) {}
 
 
     // Returns true when the command should end.
     @Override
-    public boolean isFinished()
-    {
+    public boolean isFinished() {
         return false;
     }
 }
